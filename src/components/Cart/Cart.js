7040df69b74e5b5bd7dfbcc2726077a4602a0386
Sinshi Smith <yorfnd@gmail.com>
2019-01-21 20:09:31 +0200

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { ProductConsumer } from '../../context';
+import Title from '../Title';
+import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
+import CartList from './CartList';
+import CartTotals from './CartTotals';
+
+export default class Cart extends Component {
+  render() {
+    return (
+      <section>
+        <ProductConsumer>
+          {value => {
+            const {cart} = value;
+            return (
+              (cart && cart.length > 0) ?
+              (
+                <>
+                  <Title name="your" title="Cart" />
+                  <CartColumns />
+                  <CartList value={value} />
+                  <CartTotals value={value} />
+                </>
+              ) :
+              <EmptyCart />
+            )
+
+          }}
+        </ProductConsumer>
+      </section>
+    )
+  }
+}
